230b3cb7efe806775dc55a78558795b8232c9b9a
Henry <henry.cantrell1997@outlook.com>
2020-07-29 19:51:43 -0400

    small stuff

--- src/components/mainpage/profilebox.js
+++ src/components/mainpage/profilebox.js
@@ -27,11 +27,13 @@ export let USER_PROFILE_BOTTOM_LEFT_BOX = (props) => {
 
     let userName = useSelector((state) => state.userName)
 
+    //where blb = bottom left box 
+
     return (
         <div id='blbMaster'>
             <div id='profileNameBlb'>
             </div>
-            <div id='emailBlb'>
+            <div id='usernameBlb'>
             {`@${userName}`}
             </div>
         </div>
